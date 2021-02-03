3d9d6162ce94c9d7e280295b8476fd15289a78d8
amanda-condron <67597752+amanda-condron@users.noreply.github.com>
2021-02-03 14:37:02 -0500

    Create ht.h

---
+++ ht.h
@@ -0,0 +1,22 @@
+/* ht.h */
+
+
+#ifndef _HT
+#define _HT
+
+typedef struct sHashTable  *tHashTable;
+
+/* exported functions */
+/* [[ consider first producing a header comment for each function below. ]] */
+
+tHashTable ht_initialize(int, int (*compare_function)(),int(*hash_function)());
+void ht_free(tHashTable);
+tHashTable ht_insert(tHashTable, void*);
+tHashTable ht_delete(tHashTable, void*);
+void *ht_lookup(tHashTable, void*);
+void ht_print(tHashTable, void (*print_function)());
+void ht_foreach(tHashTable, void (*function)(void *));
+void ht_foreach1(tHashTable, void (*function)(void *, void *), void*);
+void ht_foreach2(tHashTable, void (*function)(void *, void *, void *), void*, void*);
+
+#endif
