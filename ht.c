bc737dbbdad9e7481247bd7a6c4382bbc9f5c543
amanda-condron <67597752+amanda-condron@users.noreply.github.com>
2021-02-04 21:22:38 -0500

    Update ht.c

--- ht.c
+++ ht.c
@@ -1,1 +1,44 @@
+public tHashTable ht_initialize(int, int (*compare_function)(),int(*hash_function)())
+{
+  
+}
 
+public void ht_free(tHashTable)
+{
+  
+}
+
+public tHashTable ht_insert(tHashTable, void*)
+{
+  
+}
+
+public tHashTable ht_delete(tHashTable, void*)
+{
+  
+}
+
+public void *ht_lookup(tHashTable, void*)
+{
+  
+}
+
+public void ht_print(tHashTable, void (*print_function)())
+{
+  
+}
+
+public void ht_foreach(tHashTable, void (*function)(void *))
+{
+  
+}
+
+public void ht_foreach1(tHashTable, void (*function)(void *, void *), void*)
+{
+  
+}
+
+public void ht_foreach2(tHashTable, void (*function)(void *, void *, void *), void*, void*)
+{
+  
+}
