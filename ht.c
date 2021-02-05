27c86aba7fc9e835e18aebd28e75e0511bbed29d
amandacondron <acondron@loyola.edu>
2021-02-04 21:43:20 -0500

    need to figure out setup

--- ht.c
+++ ht.c
@@ -1,44 +1,50 @@
-public tHashTable ht_initialize(int, int (*compare_function)(),int(*hash_function)())
+#include "ht.h"
+
+#include "misc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+tHashTable ht_initialize(int, int (*compare_function)(),int(*hash_function)())
 {
-  
+    
 }
 
-public void ht_free(tHashTable)
+void ht_free(tHashTable)
 {
-  
+
 }
 
-public tHashTable ht_insert(tHashTable, void*)
+tHashTable ht_insert(tHashTable, void*)
 {
-  
+
 }
 
-public tHashTable ht_delete(tHashTable, void*)
+tHashTable ht_delete(tHashTable, void*)
 {
-  
+
 }
 
-public void *ht_lookup(tHashTable, void*)
+void *ht_lookup(tHashTable, void*)
 {
-  
+
 }
 
-public void ht_print(tHashTable, void (*print_function)())
+void ht_print(tHashTable, void (*print_function)())
 {
-  
+
 }
 
-public void ht_foreach(tHashTable, void (*function)(void *))
+void ht_foreach(tHashTable, void (*function)(void *))
 {
-  
+
 }
 
-public void ht_foreach1(tHashTable, void (*function)(void *, void *), void*)
+void ht_foreach1(tHashTable, void (*function)(void *, void *), void*)
 {
-  
+
 }
 
-public void ht_foreach2(tHashTable, void (*function)(void *, void *, void *), void*, void*)
+void ht_foreach2(tHashTable, void (*function)(void *, void *, void *), void*, void*)
 {
-  
+
 }
